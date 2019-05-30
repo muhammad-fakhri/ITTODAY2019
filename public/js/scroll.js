2be0bf55f4ffbe8cd6564af5716ae5976cec67de
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-05-30 20:00:56 +0700

    Update public code

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     $(document).scroll(function() {
         scroll_pos = $(this).scrollTop();
         if (scroll_pos > 100) {
-            $("header").css('background', 'linear-gradient(90deg, #F67E7D 0%, #FFB997 100%)');
+            // $("header").css('background', 'linear-gradient(90deg, #F67E7D 0%, #FFB997 100%)');
             $(".nav-menu-1").css('color', 'white');
         } else {
-            $("header").css('background', '');
-            $(".nav-menu-1").css('color', '#F67E7D');
+            // $("header").css('background', '');
+            $(".nav-menu-1").css('color', 'white');
         }
     });
 });
