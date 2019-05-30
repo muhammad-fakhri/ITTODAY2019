f3efa9f1ef0248e0d32ddc199347f1efed011826
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-05-30 20:03:12 +0700

    Update layouts view

---
+++ resources/views/layouts/main2.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ $title ?? 'IT TODAY 2019' }}</title>
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ asset('css/linearicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/hexagons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+</head>
+
+<body>
+    @include('layouts.nav', ['some' => 'data'])
+    <div class="wrapper">
+        <div class="row outer-wrapper">
+            <div class="col inner-wrapper flex">
+                <a href="http://localhost:8000"><img src="{{ asset('img/logo-picture.jpg') }}" alt="logo picture" id="logo-picture" class="flex-vertical-center"></a>
+            </div>
+            <div class="col inner-wrapper2">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+    <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/easing.min.js') }}"></script>
+    <script src="{{ asset('js/hoverIntent.js') }}"></script>
+    <script src="{{ asset('js/superfish.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/hexagons.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/mail-script.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-<header id="header" id="home">
+<header id="header" id="home" @if (isset($tipe)) style="background: linear-gradient(90deg, #F67E7D 0%, #FFB997 100%);" @endif>
     <div class="container main-menu">
         <div class="row align-items-center justify-content-between d-flex">
             <div id="logo">
@@ -6,43 +6,43 @@
             </div>
             <nav id="nav-menu-container">
                 <ul class="nav-menu">
-                    <li class="menu-active"><a href="/" style="@if (!isset($tipe))
-                    color: white;
-                @endif" class="nav-menu-1">Beranda</a></li>
-                    <li class="menu-has-children"><a style="@if (!isset($tipe))
-                    color: white;
-                    @else
-                    color:#F67E7D;
-                @endif" class="nav-menu-1">Kompetisi</a>
+                    <li class="menu-active"><a href="/" class="nav-menu-1">Beranda</a></li>
+                    <li class="menu-has-children"><a style="color: white;" class="nav-menu-1">Kompetisi</a>
                         <ul>
                             <li><a href="{{ route('apps') }}">Appstoday</a></li>
                             <li><a href="{{ route('hack') }}">Hacktoday</a></li>
                             <li><a href="{{ route('daming') }}">Data Science Competition</a></li>
                         </ul>
                     </li>
-                    <li class="menu-has-children"><a style="@if (!isset($tipe))
-                    color: white;
-                    @else
-                    color:#F67E7D;
-                @endif" class="nav-menu-1">Acara</a>
+                    <li class="menu-has-children"><a style="color: white;" class="nav-menu-1">Acara</a>
                         <ul>
                             <li><a href="{{ route('seminar') }}">Seminar</a></li>
                             <li><a href="{{ route('workshop') }}">Workshop</a></li>
                         </ul>
                     </li>
-                    <li><a href="http://himalkom.cs.ipb.ac.id" style="@if (!isset($tipe))
-                    color: white;
-                @endif" class="nav-menu-1">Himalkom IPB</a></li>
-                    <li class="menu-has-children"><a style="@if (!isset($tipe))
-                    color: white;
-                    @else
-                    color:#F67E7D;
-                @endif" class="nav-menu-1">Masuk</a>
+                    <li><a href="http://himalkom.cs.ipb.ac.id" class="nav-menu-1">Himalkom IPB</a></li>
+                    <li class="menu-has-children">
+                        @if (!isset($auth_page))
+                            {{-- expr --}}
+                        @guest
+                        <a style="color: white;" class="nav-menu-1">Masuk</a>
                         <ul>
                             <li><a href="{{ route('login') }}">Masuk</a></li>
+                            @if (Route::has('register'))
                             <li><a href="{{ route('register') }}">Daftar</a></li>
+                            @endif
                         </ul>
-                    </li>
+                        @else
+                        <a style="color: white;" class="nav-menu-1">{{ Auth::user()->name }}</a>
+                        <ul>
+                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                        @endguest
+                        @endif
                 </ul>
             </nav><!-- #nav-menu-container -->
         </div>
