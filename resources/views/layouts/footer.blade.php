aa0f4f4849865c582c4d0a0db5ad5083027bfb28
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2019-06-01 23:37:28 +0700

    aowk

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,26 +1,24 @@
 <!-- start footer Area -->
 <footer class="footer-area section-gap">
     <div class="container footer">
-        <div class="row">
+        <div class="row single-footer-widget">
             <div class="col-lg-12 col-md-12">
-                <div class="single-footer-widget">
-                    <h6>IT TODAY 2019</h6>
-                    <ul>
-                        <li>Sekretariat Himalkom IPB</li>
-                        <li>Gedung Student Center FMIPA IPB</li>
-                        <li>Jalan Meranti • Kampus IPB Dramaga • Bogor</li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>
+                        <h6>IT TODAY 2019</h6>
+                    </li>
+                    <li>Sekretariat Himalkom IPB</li>
+                    <li>Gedung Student Center FMIPA IPB</li>
+                    <li>Jalan Meranti • Kampus IPB Dramaga • Bogor</li>
+                </ul>
             </div>
         </div>
         <div class="row footer-bottom d-flex">
-            <div class="col-lg-4 col-sm-4"></div>
-            <div class="col-lg-4 col-sm-4 footer-social">
+            <div class="col-lg-12 col-md-12 footer-social">
                 <a href="https://facebook.com/ipbittoday/" target="_blank"><i class="fa fa-2x fa-facebook"></i></a>
                 <a href="https://twitter.com/ittoday_ipb" target="_blank"><i class="fa fa-2x fa-twitter"></i></a>
                 <a href="https://instagram.com/ittoday_ipb" target="_blank"><i class="fa fa-2x fa-instagram"></i></a>
             </div>
-            <div class="col-lg-4 col-sm-4"></div>
         </div>
     </div>
 </footer>
--- resources/views/layouts/main2.blade.php
+++ resources/views/layouts/main2.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="{{ asset('css/hexagons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 
 <body>
