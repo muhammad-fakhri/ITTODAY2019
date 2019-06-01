6531145bd3e0ecf4119e4f75577ad2992e866792
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2019-06-02 04:01:51 +0700

    fix verify blade

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,7 +4,7 @@
     <div class="content-verify">
         <h3>Verifikasi Email</h3>
         <p>Sebelum melanjutkan, silakan periksa email Anda untuk tautan verifikasi.
-            blautan verifikasi.</p>
+            Jika Anda tidak menerima email, klik tombol di bawah untuk mengirim ulang tautan verifikasi.</p>
         <div class="inner-verify"></div>
         <div class="btn-verify">
             <a href="{{ route('verification.resend') }}">Kirim Ulang Kode Verifikasi</a>
