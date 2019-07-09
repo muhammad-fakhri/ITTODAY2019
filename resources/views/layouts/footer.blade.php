81885aaf826a3db289b62bf6f08f6763bfeddba0
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-07-09 23:41:12 +0700

    Add logo to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,10 +3,8 @@
     <div class="container footer">
         <div class="row single-footer-widget">
             <div class="col-lg-12 col-md-12">
+                <img src="{{ asset('img/logo-footer.png') }}" class="footer-image">
                 <ul>
-                    <li>
-                        <h6>IT TODAY 2019</h6>
-                    </li>
                     <li>Sekretariat Himalkom IPB</li>
                     <li>Gedung Student Center FMIPA IPB</li>
                     <li>Jalan Meranti • Kampus IPB Dramaga • Bogor</li>
