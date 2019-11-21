8a9231d1e12b2c1a7847a9e6e5089ecb12c4fab5
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2019-11-21 16:28:10 +0700

    Add sponsor logo add footer layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,17 @@
+<!-- Start sponsor Area -->
+<section class="feature-area section-gap">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-12 pb-40 header-text text-center">
+                <h1 class="pb-10 text-red">SPONSOR IT TODAY 2019</h1>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <img src="{{ asset('img/sponsor.jpg') }}">
+        </div>
+    </div>
+</section>
+<!-- End sponsor Area -->
 <!-- start footer Area -->
 <footer class="footer-area section-gap">
     <div class="container footer">
